eed57eff08748bdd18c973ebc674d8928645031d
Matti Tuomikoski <n8tuma00@students.oamk.fi>
2019-09-18 20:32:06 +0300

    Part 2 phonebook step 4 done.

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,20 +1,49 @@
 import React, { useState } from 'react'
 
-const Numbers = (props) => {
+const Persons = (props) => {
+  const {persons, filter} = props
+  const visiblePersons = (filter.length === 0) 
+    ? persons
+    : persons.filter(person => {
+      const lowerCaseName = person.name.toLowerCase()
+      const lowerCaseFilter = filter.toLowerCase()
+      return (
+        lowerCaseName.includes(lowerCaseFilter)
+      )
+    })
+
   return (
     <ul>
-      {props.persons.map(person => 
-          <li key={person.name}>{person.name}</li>
+      {visiblePersons.map(person => 
+          <li key={person.name}>{person.name} {person.number}</li>
       )}
     </ul>
   )
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { 
+      name: 'Arto Hellas', 
+      number: '040-123456' 
+    },
+    { 
+      name: 'Ada Lovelace', 
+      number: '39-44-5323523' 
+    },
+    { 
+      name: 'Dan Abramov', 
+      number: '12-43-234345' 
+    },
+    { 
+      name: 'Mary Poppendieck', 
+      number: '39-23-6423122' 
+    }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -22,32 +51,47 @@ const App = () => {
     if (persons.find(person => person.name === newName) !== undefined) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({ name: newName }))
+      setPersons(persons.concat({ name: newName, number: newNumber }))
     }
-
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter names with&nbsp;
+        <input
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <h2>Add new data</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: 
+          Name:&nbsp;
           <input 
             value={newName} 
             onChange={handleNameChange}
           />
         </div>
         <div>
-          <button type="submit">add</button>
+          Number:&nbsp;
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Persons persons={persons} filter={filter} />
     </div>
   )
 }
