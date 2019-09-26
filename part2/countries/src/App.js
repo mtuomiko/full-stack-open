25c5847e5c8c7f05ab1ccda879a85aa13335118b
Matti Tuomikoski <n8tuma00@students.oamk.fi>
2019-09-26 21:58:23 +0300

    Refactored so countries exercise App.js is in source root, not in components folder

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Filter from './Filter'
-import Countries from './Countries'
+import Filter from './components/Filter'
+import Countries from './components/Countries'
 
 const App = () => {
   const [filter, setFilter] = useState('')
