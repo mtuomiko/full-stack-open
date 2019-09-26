d66751a261c42cdd7a5eef5e1b95a7793eab8c92
Matti Tuomikoski <n8tuma00@students.oamk.fi>
2019-09-26 22:03:50 +0300

    Refactored part 2 phonebook so App.js is in source root, not in components folder

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import Filter from './Filter'
-import PersonForm from './PersonForm'
-import Persons from './Persons'
-import personService from '../services/persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
