dd1c3efb0a4d65968bb5b4409dfb7c4bbe826af2
Matti Tuomikoski <n8tuma00@students.oamk.fi>
2019-10-15 20:32:59 +0300

    Proceeded to 4.16 bloglist expansion, step 5

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,18 +4,41 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const morgan = require('morgan')
 
 const app = express()
+
+logger.info('Connecting to MongoDB at ', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
+  useCreateIndex: true,
 })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
 
+morgan.token('request-body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
+
+// Using morgan for logging
+//app.use(middleware.requestLogger)
+
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
